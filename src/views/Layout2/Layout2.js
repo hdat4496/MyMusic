8e9c5c8187ce9038c73d0b99e1ea89727e3f0c31
hoanghouit <hohoang.ag.96@gmail.com>
2018-11-04 00:55:46 +0700

    [ChartPage] Update some chart UI

--- src/views/Layout2/Layout2.js
+++ src/views/Layout2/Layout2.js
@@ -45,7 +45,7 @@ const mainChart = {
   labels: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'Acousticness',
       backgroundColor: hexToRgba(brandInfo, 10),
       borderColor: brandInfo,
       pointHoverBackgroundColor: '#fff',
@@ -53,7 +53,7 @@ const mainChart = {
       data: data1,
     },
     {
-      label: 'My Second dataset',
+      label: 'Danceability',
       backgroundColor: 'transparent',
       borderColor: brandSuccess,
       pointHoverBackgroundColor: '#fff',
@@ -61,7 +61,7 @@ const mainChart = {
       data: data2,
     },
     {
-      label: 'My Third dataset',
+      label: 'Energy',
       backgroundColor: 'transparent',
       borderColor: brandDanger,
       pointHoverBackgroundColor: '#fff',
@@ -118,24 +118,95 @@ const mainChartOpts = {
 
 //==============================//============================
 
-const doughnut = {
+const chart_key_data = {
   labels: [
-    'Red',
-    'Green',
-    'Yellow',
+    'C',
+    'C#',
+    'D',
+    'D#',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'A#',
+    'B',
   ],
   datasets: [
     {
-      data: [300, 50, 100],
+      data: [40, 40, 10, 0, 30, 20, 20, 30, 120, 60, 10, 40],
+      backgroundColor: [
+        '#ff4444',
+        '#CC0000',
+        '#ffbb33',
+        '#FF8800',
+        '#00C851',
+        '#33b5e5',
+        '#0099CC',
+        '#2BBBAD',
+        '#00695c',
+        '#aa66cc',
+        '#9933CC',
+        '#2E2E2E',
+      ],
+      hoverBackgroundColor: [
+        '#ff4444',
+        '#CC0000',
+        '#ffbb33',
+        '#FF8800',
+        '#00C851',
+        '#33b5e5',
+        '#0099CC',
+        '#2BBBAD',
+        '#00695c',
+        '#aa66cc',
+        '#9933CC',
+        '#2E2E2E',
+      ],
+    }],
+};
+
+const chart_mode_data = {
+  labels: [
+    'Minor',
+    'Major',
+  ],
+  datasets: [
+    {
+      data: [35, 65],
       backgroundColor: [
         '#FF6384',
         '#36A2EB',
-        '#FFCE56',
       ],
       hoverBackgroundColor: [
         '#FF6384',
         '#36A2EB',
+      ],
+    }],
+};
+
+const chart_time_signature_data = {
+  labels: [
+    '1 beat',
+    '2 beat',
+    '3 beat',
+    '4 beat'
+  ],
+  datasets: [
+    {
+      data: [35, 65, 20, 49],
+      backgroundColor: [
+        '#FF6384',
+        '#36A2EB',
         '#FFCE56',
+        '#00C851',
+      ],
+      hoverBackgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FF6384',
+        '#00C851',
       ],
     }],
 };
@@ -147,7 +218,7 @@ const line = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'Sample data',
       fill: false,
       lineTension: 0.1,
       backgroundColor: 'rgba(75,192,192,0.4)',
@@ -169,6 +240,55 @@ const line = {
     },
   ],
 };
+
+const multi_line = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Sample data',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+    {
+      label: 'Sample data 1',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(233, 30, 99, 0.7)',
+      borderColor: brandDanger,
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: brandDanger,
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: brandDanger,
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [45, 69, 30, 51, 76, 52, 45],
+    },
+  ],
+};
+
 const options = {
   tooltips: {
     enabled: false,
@@ -208,14 +328,48 @@ class Layout2 extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
-          <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
-              <CardBody>
-                <div className="chart-wrapper">
-                  <Doughnut data={doughnut} />
-                </div>
-              </CardBody>
-            </Card>
+          <Col xs="4" sm="4" style={{ margin: '10px auto' }}>
+
+            <Row>
+              <Col xs="12">
+                <Card style={{ width: '100%' }}>
+                  <CardHeader>
+                    Mode
+                    <div className="card-header-actions">
+                      <a href="http://www.chartjs.org" className="card-header-action">
+                        <small className="text-muted">docs</small>
+                      </a>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <div className="chart-wrapper">
+                      <Doughnut data={chart_mode_data} options={options} />
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+
+                <Card style={{ width: '100%' }}>
+                  <CardHeader>
+                    Time signature
+              <div className="card-header-actions">
+                      <a href="http://www.chartjs.org" className="card-header-action">
+                        <small className="text-muted">docs</small>
+                      </a>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <div className="chart-wrapper" >
+                      <Doughnut data={chart_time_signature_data} options={options} />
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+
           </Col>
 
           <Col xs="8" sm="8" style={{ margin: '10px auto', display: 'flex' }}>
@@ -224,14 +378,13 @@ class Layout2 extends Component {
 
                 <Row>
                   <Col sm="5">
-                    <CardTitle className="mb-0">Traffic</CardTitle>
-                    <div className="small text-muted">November 2015</div>
+                    <CardTitle className="mb-0">Rhythm</CardTitle>
+                    <div className="small text-muted">April 2018</div>
                   </Col>
                   <Col sm="7" className="d-none d-sm-inline-block">
-                    <Button color="primary" className="float-right"><i className="icon-cloud-download"></i></Button>
                     <ButtonToolbar className="float-right" aria-label="Toolbar with button groups">
                       <ButtonGroup className="mr-3" aria-label="First group">
-                        <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(1)} active={this.state.radioSelected === 1}>Day</Button>
+                        <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(1)} active={this.state.radioSelected === 1}>Week</Button>
                         <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(2)} active={this.state.radioSelected === 2}>Month</Button>
                         <Button color="outline-secondary" onClick={() => this.onRadioBtnClick(3)} active={this.state.radioSelected === 3}>Year</Button>
                       </ButtonGroup>
@@ -245,29 +398,19 @@ class Layout2 extends Component {
               <CardFooter>
                 <Row className="text-center">
                   <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">Visits</div>
-                    <strong>29.703 Users (40%)</strong>
+                    <div className="text-muted">Acousticness</div>
+                    <strong>2703 songs (40%)</strong>
                     <Progress className="progress-xs mt-2" color="success" value="40" />
                   </Col>
                   <Col sm={12} md className="mb-sm-2 mb-0 d-md-down-none">
-                    <div className="text-muted">Unique</div>
-                    <strong>24.093 Users (20%)</strong>
+                    <div className="text-muted">Danceability</div>
+                    <strong>293 songs (20%)</strong>
                     <Progress className="progress-xs mt-2" color="info" value="20" />
                   </Col>
                   <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">Pageviews</div>
-                    <strong>78.706 Views (60%)</strong>
-                    <Progress className="progress-xs mt-2" color="warning" value="60" />
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">New Users</div>
-                    <strong>22.123 Users (80%)</strong>
-                    <Progress className="progress-xs mt-2" color="danger" value="80" />
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0 d-md-down-none">
-                    <div className="text-muted">Bounce Rate</div>
-                    <strong>Average Rate (40.15%)</strong>
-                    <Progress className="progress-xs mt-2" color="primary" value="40" />
+                    <div className="text-muted">Energy</div>
+                    <strong>706 songs (60%)</strong>
+                    <Progress className="progress-xs mt-2" color="danger" value="60" />
                   </Col>
                 </Row>
               </CardFooter>
@@ -277,39 +420,56 @@ class Layout2 extends Component {
 
         <Row>
           <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
-              <CardBody style={{height: '100%'}}>
-                <Row style={{height: '100%'}}>
-                  <Col  style={{margin:'auto'}}  xs="6" sm="6">
-                    <div className="chart-wrapper">
-                      <Doughnut data={doughnut} />
-                    </div>
-                  </Col>
-                  <Col   style={{margin:'auto'}} xs="6" sm="6">
-                    <div className="chart-wrapper">
-                      <Doughnut data={doughnut} />
-                    </div>
-                  </Col>
-                </Row>
+            <Card style={{ width: '100%' }}>
+              <CardHeader>
+                Key
+              <div className="card-header-actions">
+                  <a href="http://www.chartjs.org" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
+              <CardBody style={{ height: '100%' }}>
+
+                <div className="chart-wrapper">
+                  <Pie data={chart_key_data} />
+                </div>
+
               </CardBody>
             </Card>
           </Col>
 
           <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
+            <Card style={{ width: '100%' }}>
+              <CardHeader>
+                Tempo
+              <div className="card-header-actions">
+                  <a href="http://www.chartjs.org" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
               <CardBody>
-              <div className="chart-wrapper">
-                <Line data={line} options={options} />
-              </div>
+                <div className="chart-wrapper">
+                  <Line data={line} options={options} />
+                </div>
               </CardBody>
             </Card>
           </Col>
           <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
+            <Card style={{ width: '100%' }}>
+              <CardHeader>
+                Vocality
+              <div className="card-header-actions">
+                  <a href="http://www.chartjs.org" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
               <CardBody>
-              <div className="chart-wrapper">
-                <Line data={line} options={options} />
-              </div>
+                <div className="chart-wrapper">
+                  <Line data={multi_line} options={options} />
+                </div>
               </CardBody>
             </Card>
           </Col>
@@ -318,30 +478,54 @@ class Layout2 extends Component {
 
         <Row>
           <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
+            <Card style={{ width: '100%' }}>
+              <CardHeader>
+                Duration
+              <div className="card-header-actions">
+                  <a href="http://www.chartjs.org" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
               <CardBody>
-              <div className="chart-wrapper">
-                <Line data={line} options={options} />
-              </div>
+                <div className="chart-wrapper">
+                  <Line data={line} options={options} />
+                </div>
               </CardBody>
             </Card>
           </Col>
 
           <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
+            <Card style={{ width: '100%' }}>
+              <CardHeader>
+                Valence
+              <div className="card-header-actions">
+                  <a href="http://www.chartjs.org" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
               <CardBody>
-              <div className="chart-wrapper">
-                <Line data={line} options={options} />
-              </div>
+                <div className="chart-wrapper">
+                  <Line data={line} options={options} />
+                </div>
               </CardBody>
             </Card>
           </Col>
           <Col xs="4" sm="4" style={{ margin: '10px auto', display: 'flex' }}>
-          <Card style={{ width: '100%' }}>
+            <Card style={{ width: '100%' }}>
+              <CardHeader>
+                Loudness
+              <div className="card-header-actions">
+                  <a href="http://www.chartjs.org" className="card-header-action">
+                    <small className="text-muted">docs</small>
+                  </a>
+                </div>
+              </CardHeader>
               <CardBody>
-              <div className="chart-wrapper">
-                <Line data={line} options={options} />
-              </div>
+                <div className="chart-wrapper">
+                  <Line data={line} options={options} />
+                </div>
               </CardBody>
             </Card>
           </Col>
